754e4da883517cf1a89dcbf1035b2504e1b943be
Labing I5 <Labing I5@LabingI5-0-101>
2017-11-01 10:58:51 -0500

    Configuracion Local

---
+++ src/main/java/com/crunchify/jsp/servlet/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crunchify.jsp.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author Labing I5
+ */
+public class NewClass extends HttpServlet{
+    
+    
+    
+}
--- src/main/java/edu/co/sergio/mundo/dao/Conexion.java
+++ src/main/java/edu/co/sergio/mundo/dao/Conexion.java
@@ -7,14 +7,22 @@ public class Conexion {
 	
 	private static Connection CONEXION=null;
     	public static Connection getConnection() throws URISyntaxException{
-            URI dbUri = new URI(System.getenv("DATABASE_URL"));
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        String HOST = "-----:----"; 
+        String DATABASE = "----";
+        String USER = "----";
+        String PASS = "----";
+
+//      URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//      String username = dbUri.getUserInfo().split(":")[0];
+//      String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" +
+                HOST + "/" + DATABASE +
+                "?user=" + USER  + "&password=" + 
+                PASS + "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
     
 		   if(CONEXION == null){
 			  	try {
-					CONEXION = DriverManager.getConnection(dbUrl, username, password);
+					CONEXION = DriverManager.getConnection(dbUrl);
                         	} catch (SQLException e) {
 					System.out.println("Connection Failed! Check output console");
 					e.printStackTrace();
